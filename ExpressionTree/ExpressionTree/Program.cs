0660e18dc2d8c810c75399226c614ac7f7acd368
W三五八团楚云飞 <2426566602@qq.com>
2023-05-09 17:13:34 +0800

    feat:

---
+++ ExpressionTree/ExpressionTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string> a = ()=>"hello world";
+            Console.WriteLine(a.Invoke());
+           Expression<Func<string>> exception=()=> "hello world";
+            
+            Console.WriteLine(exception.Compile().Invoke());
+        }
+    }
+}
