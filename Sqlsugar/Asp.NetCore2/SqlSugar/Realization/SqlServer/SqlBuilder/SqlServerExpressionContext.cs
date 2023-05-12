a5b0982349262d68a4abd02e8ee3e34d4e4a5aab
W三五八团楚云飞 <2426566602@qq.com>
2023-05-12 08:33:59 +0800

    feat:上传源码

---
+++ Sqlsugar/Asp.NetCore2/SqlSugar/Realization/SqlServer/SqlBuilder/SqlServerExpressionContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugar
+{
+    public partial class SqlServerExpressionContext : ExpressionContext, ILambdaExpressions
+    {
+        public SqlSugarProvider Context { get; set; }
+        public SqlServerExpressionContext()
+        {
+            base.DbMehtods = new SqlServerMethod();
+        }
+
+    }
+    public partial class SqlServerMethod : DefaultDbMethod, IDbMethods
+    {
+        public override string GetForXmlPath()
+        {
+            return "  FOR XML PATH('')),1,len(N','),'')  ";
+        }
+        public override string GetStringJoinSelector(string result, string separator)
+        {
+            return $"stuff((SELECT cast(N'{separator}' as nvarchar(max)) + cast({result} as nvarchar(max))";
+        }
+        public override string DateValue(MethodCallExpressionModel model)
+        {
+            var parameter = model.Args[0];
+            var parameter2 = model.Args[1];
+            if (parameter.MemberName != null && parameter.MemberName is DateTime)
+            {
+                return string.Format(" datepart({0},'{1}') ", parameter2.MemberValue, parameter.MemberName);
+            }
+            else
+            {
+                return string.Format(" datepart({0},{1}) ", parameter2.MemberValue, parameter.MemberName);
+            }
+        }
+        public override string HasValue(MethodCallExpressionModel model)
+        {
+            if (model.Args[0].Type == UtilConstants.GuidType)
+            {
+                var parameter = model.Args[0];
+                return string.Format("( {0} IS NOT NULL )", parameter.MemberName);
+            }
+            else
+            {
+                var parameter = model.Args[0];
+                return string.Format("( {0}<>'' AND {0} IS NOT NULL )", parameter.MemberName);
+            }
+        }
+
+        public override string JsonField(MethodCallExpressionModel model)
+        {
+            var parameter = model.Args[0];
+            var parameter1 = model.Args[1];
+            //var parameter2 = model.Args[2];
+            //var parameter3= model.Args[3];
+            var result = GetJson(parameter.MemberName, parameter1.MemberName, model.Args.Count() == 2);
+            if (model.Args.Count > 2)
+            {
+                result = GetJson(result, model.Args[2].MemberName, model.Args.Count() == 3);
+            }
+            if (model.Args.Count > 3)
+            {
+                result = GetJson(result, model.Args[3].MemberName, model.Args.Count() == 4);
+            }
+            if (model.Args.Count > 4)
+            {
+                result = GetJson(result, model.Args[4].MemberName, model.Args.Count() == 5);
+            }
+            if (model.Args.Count > 5)
+            {
+                result = GetJson(result, model.Args[5].MemberName, model.Args.Count() == 6);
+            }
+            return result;
+        }
+
+        private string GetJson(object memberName1, object memberName2, bool isLast)
+        {
+            return $"JSON_VALUE({memberName1}, '$.'+"+memberName2+")";
+        }
+    }
+}
