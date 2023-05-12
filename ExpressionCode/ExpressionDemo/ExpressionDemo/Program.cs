352779d54265beacd1868934eaae9c90d4a40d09
W三五八团楚云飞 <2426566602@qq.com>
2023-05-12 08:19:50 +0800

    fixme:调整代码结构

---
+++ ExpressionCode/ExpressionDemo/ExpressionDemo/Program.cs
@@ -0,0 +1,81 @@
+﻿using ExpressionDemo.MappingExtend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionDemo
+{
+    /// <summary>
+    /// 1 什么是表达式目录树Expression
+    /// 2 动态拼装Expression
+    /// 3 基于Expression扩展应用
+    /// 
+    /// 1 ExpressionVisitor解析表达式目录树
+    /// 2 解析Expression生成Sql
+    /// 3 Expression扩展拼装链接
+    /// 
+    /// Sql查询的时候，各种查询条件呢？没有实现  写不出来，
+    /// 属性可能是ID  name  account state
+    /// 值 可能是int string  datetime
+    /// 大于 小于  等于  包含
+    /// 一个条件  2个条件 N个条件
+    /// 不要说传递sql语句id=3
+    /// 土办法  封装了一个对象   column  operation  value     接受一个集合
+    /// 表达式目录树，数据结构
+    /// 调用方会有各种的条件需要传递下去
+    /// 底层需要解析调用方传递的东西
+    /// 所以需要一个数据结构(语法/约定)，上端去组装，下端去解析
+    /// 
+    /// 表达式目录树就是数据结构！！！ 一个能拼装能解析的数据结构
+    /// 
+    /// 
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                People people = new People()
+                {
+                    Id = 11,
+                    Name = "CIM",
+                    Age = 31
+                };
+                PeopleCopy peopleCopy = ExpressionGenericMapper<People, PeopleCopy>.Trans(people);
+                //{
+                //    new List<int>().Where(i => i > 10);
+                //    new List<int>().AsQueryable().Where(i => i > 10);
+                //}
+                {
+                    Console.WriteLine("****************认识表达式目录树*************");
+                    //ExpressionTest.Show();
+                }
+                {
+                    Console.WriteLine("********************MapperTest********************");
+                    ExpressionTest.MapperTest();
+                }
+                {
+                    Console.WriteLine("********************解析表达式目录树********************");
+                   //ExpressionVisitorTest.Show();
+                    //ExpressionVisitor访问者类，Visit是个入口，解读表达式的入口
+                    //1 lambda会区分参数和方法体，调度到更专业访问(处理)方法
+                    //2 根据表达式的类型，将表达式调度到此类中更专用的访问方法之一的表达式
+                    //默认的专业处理方法就是按照旧的模式产生一个新的
+                    //也可以自行扩展，把一些解读操作进行变化
+                    //表达式目录树是个二叉树，visit做的事儿就是一层层的解析下去，一直到最终的叶节点
+                }
+                //用linq to sql
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ExpressionCode/ExpressionDemo/ExpressionDemo/Visitor/OperationsVisitor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionDemo.Visitor
+{
+    /// <summary>
+    /// 表达式目录树访问者扩展，遇到Add换成Subtract
+    /// </summary>
+    public class OperationsVisitor : ExpressionVisitor
+    {
+        public Expression Modify(Expression expression)
+        {
+            return this.Visit(expression);
+        }
+
+        /// <summary>
+        /// Visit是个入口，解读node表示式
+        /// 根据表达式的类型，将表达式调度到此类中更专用的访问方法之一的表达式
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public override Expression Visit(Expression node)
+        {
+            Console.WriteLine($"Visit {node.ToString()}");
+            return base.Visit(node);
+        }
+        protected override Expression VisitBinary(BinaryExpression b)
+        {
+            Expression left = this.Visit(b.Left);
+            Expression right = this.Visit(b.Right);
+            if (b.NodeType == ExpressionType.Add)
+            {
+                return Expression.Subtract(left, right);
+            }
+            else if (b.NodeType == ExpressionType.Multiply)
+            {
+                //Expression left = this.Visit(b.Left);//可能还是需要进一步解析的
+                //Expression right = this.Visit(b.Right);
+                return Expression.Divide(left, right);
+
+                //return Expression.Divide(b.Left, b.Right);
+            }
+            Expression result = base.VisitBinary(b);//默认的二元访问，其实什么都不干
+            return result;
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            return base.VisitConstant(node);
+        }
+    }
+}
